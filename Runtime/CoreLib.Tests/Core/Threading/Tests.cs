57192714343d637e436cf7b553c1f45e7b3d4684
n9 <n9>
2014-03-27 20:28:02 +0100

    added ThreadingTests stub

---
+++ Runtime/CoreLib.Tests/Core/Threading/Tests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CoreLib.Tests.Core.Threading
+{
+    [TestFixture]
+    public class ThreadingTests : CoreLibTestBase {}
+}
